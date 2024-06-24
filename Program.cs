3a5d876c46e4618f291f66811f3c158539b89bb2
Crowler 1337 <gamevax25@gmail.com>
2024-06-24 15:44:53 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Number matcher");
+Console.WriteLine();
+
+Console.WriteLine("Number needs to match form of Russian Federation (Example: +7(9хх)ххх-хх-хх or 8(9хх)ххх-хх-хх)");
+Console.WriteLine("Input number");
+string userInput = Console.ReadLine();
+
+string pattern = @"^(8|\+7)\(9\d{2}\)\d{3}(-\d{2}){2}$";
+
+Regex regex = new(pattern, RegexOptions.IgnoreCase);
+Match match = regex.Match(userInput);
+
+if (match.Success)
+    Console.WriteLine("Congrats, it's match");
+else
+    Console.WriteLine("Doesn't match");
